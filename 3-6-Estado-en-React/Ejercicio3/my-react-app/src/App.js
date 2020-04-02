b68bd3cd331c7acba718d0b9c8f25c6651a83be3
Margui <margui.berjon@gmail.com>
2020-04-02 18:59:06 +0200

    3.6 3

---
+++ 3-6-Estado-en-React/Ejercicio3/my-react-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Clock from './Clock';
+
+
+
+
+function App() {
+
+  return <Clock></Clock>
+}
+
+export default App;
+
---
+++ 3-6-Estado-en-React/Ejercicio3/my-react-app/src/Clock.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.date = new Date();
+
+    this.state = {
+      hours: this.date.getHours(),
+      minutes: this.date.getMinutes(),
+      seconds: this.date.getSeconds()
+
+    };
+
+    setInterval(() => { this.updateClock() }, 1000);
+
+    this.updateClock = this.updateClock.bind(this);
+  }
+
+  updateClock() {
+    const date = new Date();
+    this.setState({
+      hours: date.getHours(),
+      minutes: date.getMinutes(),
+      seconds: date.getSeconds()
+    })
+
+  }
+
+
+  render() {
+    return (
+      <div>
+        <p>Son las: {this.state.hours}:{this.state.minutes}:{this.state.seconds}
+        </p>
+
+      </div>
+    );
+  }
+}
+
+
+export default Clock;
