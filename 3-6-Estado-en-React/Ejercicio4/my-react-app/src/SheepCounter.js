46fbc945fbe126913a55027da48f6ef1c41c6dea
Margui <margui.berjon@gmail.com>
2020-04-02 19:29:14 +0200

    3.6 4

---
+++ 3-6-Estado-en-React/Ejercicio4/my-react-app/src/SheepCounter.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+
+class SheepCounter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0
+    };
+    this.upDateCounter = this.upDateCounter.bind(this);
+  }
+
+
+  upDateCounter() {
+    this.setState(() => {
+      let nextValue = this.state.counter + 1;
+      return {
+        counter: nextValue
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div className="counter">
+        <p className="number">{this.state.counter}</p>
+        <button className="button" onClick={this.upDateCounter}>
+          Botón
+        </button>
+      </div>
+    );
+  }
+}
+
+
+export default SheepCounter;
