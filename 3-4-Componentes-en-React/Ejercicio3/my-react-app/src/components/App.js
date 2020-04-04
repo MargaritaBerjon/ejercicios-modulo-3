7de3100fe5cad0dcd43d0e91404fc4001469729d
Margui <margui.berjon@gmail.com>
2020-04-04 17:37:50 +0200

    3.4 3

---
+++ 3-4-Componentes-en-React/Ejercicio3/my-react-app/src/components/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import '../App.css';
+import { ItemList } from './ItemList';
+
+const items = [
+  {
+    name: 'Cereales con chocolate',
+    description: 'Cereales rellenos de chocolate',
+    quantity: 2,
+    category: 'Cereales',
+    price: 5
+  },
+  {
+    name: 'Hamburguesa con queso',
+    description: 'Hamburguesa rica y saludable',
+    quantity: 1,
+    category: 'Fast-food',
+    price: 15
+  },
+  {
+    name: 'Agua mineral',
+    quantity: 2,
+    category: 'Bebida',
+    price: 5
+  }
+];
+
+function App() {
+  return (
+    <div className="App">
+      <ItemList items={items} />
+    </div>
+  );
+}
+
+
+
+
+export default App;
---
+++ 3-4-Componentes-en-React/Ejercicio3/my-react-app/src/components/Item.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Item(props) {
+  return (
+    <div className="item">
+      <h5 className="quantity">{props.quantity}</h5>
+      <div>
+        <h5>{props.name}</h5>
+        <h6 className="text-muted">{props.description}</h6>
+      </div>
+      <div className="badge badge-info">{props.category}</div>
+      <h5 className="price">{props.price}€</h5>
+    </div>
+  );
+}
+
+Item.defaultProps = {
+  description: 'No hay descripción'
+};
+
+export default Item;
