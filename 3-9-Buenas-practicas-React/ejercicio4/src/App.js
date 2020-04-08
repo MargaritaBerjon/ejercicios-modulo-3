e5e6adc2bd0394be0657142176abef5d8d1ac643
Margui <margui.berjon@gmail.com>
2020-04-08 20:29:13 +0200

    3.9 4

---
+++ 3-9-Buenas-practicas-React/ejercicio4/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+const users = [
+  { name: 'Nymphadora Tonks', time: 9 },
+  { name: 'Cedric Diggory', time: 28 },
+  { name: 'Cho Chang', time: 35 },
+  { name: 'Luna Lovegood', time: 45 },
+  { name: 'Gregory Goyle', time: 56 }
+];
+
+
+function App() {
+  return (
+    <div className="App">
+      {
+        users.slice(0, 3).map(({ name, time }, i) => <p key={i}>{name}-{time}</p>)
+      }
+    </div>
+  );
+}
+
+export default App;
